4f834a7727f044050ef2bd7ea7c92064bf3ad8dc
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-27 21:51:10 +0100

    feature: add product detail view

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import product_list
+from .views import product_list, product_detail
 
 app_name = 'catalogues'
+
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('<slug:category_slug>', product_list, name='product_list_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>', product_detail, name='product_detail'),
 ]
