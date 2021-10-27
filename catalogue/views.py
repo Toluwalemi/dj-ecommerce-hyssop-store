2535ccb4d06914ee22711956283ce5e46a120374
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-27 21:06:49 +0100

    feature: add product-list views

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from catalogue.models import Category, Product
+
+
+def product_list(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    return render(
+        request,
+        'product/list.html',
+        {
+            'categories': categories,
+            'products': products
+        }
+    )
