67b44de975bc7b0427d95941c359150c435d1641
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-27 20:33:34 +0100

    feature: create models

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from catalogue.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'slug', 'price', 'available')
+    list_filter = ('category', 'available')
+    list_editable = ('price', 'available')
+    prepopulated_fields = {'slug': ('name',)}
