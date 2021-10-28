0fe9327b32c53c84710b27363f6a90a8e6c09f1f
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-28 19:42:12 +0100

    feature: add cart app

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalogue',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_ID = 'cart'
