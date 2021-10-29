409ff52b6efb49e5882fc534e1f581b55e6468fe
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-29 16:30:15 +0100

    feature: add path for orders

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogue.urls', namespace='catalogues')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('cart.urls', namespace='orders')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
